6f3cbe7e06a8928266066880c6a7188da2855142
02260616 <114304554+02260616@users.noreply.github.com>
2024-08-07 20:53:11 +0800

    lab6

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,7 +63,6 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
-void            k_incre_ref(void *);
 
 // log.c
 void            initlog(int, struct superblock*);
@@ -171,8 +170,6 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-uint64*         walk(pagetable_t, uint64, int);
-
 
 // plic.c
 void            plicinit(void);
--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,14 +23,10 @@ struct {
   struct run *freelist;
 } kmem;
 
-uint64 reference_count[(PHYSTOP-KERNBASE)/PGSIZE];//记录物理页面的引用计数
-
-
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
-  memset(reference_count, 0, sizeof(reference_count));
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -39,10 +35,8 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
-    k_incre_ref(p);
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
-  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -57,10 +51,6 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-
-  uint64 index = ((uint64)pa-PGROUNDUP((uint64)end)) / PGSIZE;
-  if(--reference_count[index] != 0)
-    return;
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -82,23 +72,11 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r){
+  if(r)
     kmem.freelist = r->next;
-    uint64 index = (PGROUNDUP((uint64)r)-PGROUNDUP((uint64)end)) / PGSIZE;
-    reference_count[index] = 1;
-  }
   release(&kmem.lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
-    
   return (void*)r;
-}
-
-//增加给定物理页面的引用计数
-void
-k_incre_ref(void* pa)
-{
-  uint64 index = (PGROUNDUP((uint64)pa)-PGROUNDUP((uint64)end)) / PGSIZE;
-  reference_count[index]++;
 }
